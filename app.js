5b571bd2f59631292c455396a3c8cbdf438e6cf2
Amotul-raheem <amotulraheem1499@gmail.com>
2022-01-27 01:45:25 +0100

    created a signup end point installed new dependencies created a .env file

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
+// import dotenv from 'dotenv';
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+
+
+
+// const connection = require("./db")
+
+//database connection
+// connection()
+// dotenv.config();
 
 const app = express();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 
-const CONNECTION_URL = 'mongodb+srv://park-booking-system:parkbooking2022@cluster0.sk0vq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://booking:booking@cluster0.sk0vq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000
-
 
 try{
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true},
@@ -17,3 +25,21 @@ mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: tru
 } catch (error){
     console.log(error.message)
 }
+
+// app.get("/", (req,res) => {
+//     res.send("hello World")
+// });
+
+app.post("/signup", (req,res) => {
+    const{password,email} = req.body;
+    console.log(password,email);
+
+    res.send("hello World")
+});
+
+
+
+
+app.listen(5000, (req,res) => {
+    console.log(`server is running on port ${PORT}`)
+});
