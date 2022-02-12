4e5224386d398b37cae4a3c78a9f2827acb786a7
folaneem <folaneem@gmail.com>
2022-02-12 17:15:52 +0100

    created the sign out endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -69,5 +69,25 @@ authRouter.post("/sign-in", async (req, res) => {
         res.status(500).send(error);
     }
 });
+// sign out Route
 
+authRouter.get('/api/profile',auth,function (req,res){
+    res.json({
+        isAuth: true,
+        id: req.user._id,
+        email: req.user.email
+
+    })
+});
+
+
+authRouter.get("/sign-out", auth,function(req,res) {
+        req.user.deleteToken(req.token,(err,user)=>{
+            if(err) {
+                return res.status(400).send(err);
+                res.sendStatus(200);
+            }
+        })
+
+})
 export {authRouter}
