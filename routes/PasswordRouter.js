152b8542089a5cd3ae8ccd9f296c5fc786ea5318
Amotul-raheem <amotulraheem1499@gmail.com>
2022-02-23 08:47:45 +0100

    Added comments to code.

--- routes/PasswordRouter.js
+++ routes/PasswordRouter.js
@@ -15,23 +15,31 @@ const emailValidator = joi.object({
 
 passwordRouter.post("/forgot-password", async (req, res) => {
     try {
+        //Checks that email sent by user is a valid email.
         const {error} = await emailValidator.validateAsync(req.body);
         if (error) {
             res.status(400).send(error.details[0].message)
         }
+
+        //Checks if the user with email exists in database.
         const user = await User.findOne({email: req.body.email});
         if (!user) {
             res.status(400).send("Incorrect Email");
         }
+
+        //Generates a random token that is unique to te user.
         const resetToken = uuidv4();
 
+        //Adds neww fields to the userSchema; reset-token and reset-token creation date
         user.reset_token = resetToken
         user.reset_token_creation_date = Date.now();
-
         await user.save()
 
         const link = `${process.env.BASE_URL}/reset-password/${resetToken}`;
+
+        //Sends reset password link using axios
         sendResetPasswordEmail(link, user.email, user.username)
+
         res.status(200).send("Password reset link sent to your email");
     } catch (error) {
         res.status(500).send(error);
@@ -45,20 +53,26 @@ const passwordValidator = joi.object({
 
 passwordRouter.post("/reset-password/:resetToken", async (req, res) => {
     try {
+        //Validates password using joi validation
         const {error} = await passwordValidator.validateAsync(req.body);
         if (error) {
             res.status(400).send(error.details[0].message)
         }
 
+        //Checks if user's token exists in database
         const user = await User.findOne({reset_token: req.params.resetToken});
         if (!user) return res.status(400).send("Invalid Token");
 
+        //Checks if token has expired
         if (isTokenExpired(user.reset_token_creation_date)) {
             res.status(401).send("Link provided has expired")
         }
 
+        //Encrypts user's new password
         const salt = await bcrypt.genSalt(10);
         user.password = await bcrypt.hash(req.body.password, salt);
+
+        //Sets user's token and creation date to default
         user.reset_token_creation_date = null;
         user.reset_token = null;
         await user.save();
