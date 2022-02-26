ac7918fff7c9c1a67614e48c7ccf582f9eb4b903
Olajide Abdulrazzaq Folarin <folaraz11@gmail.com>
2022-02-26 11:50:24 +0000

    added cors

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ import {authenticationRouter} from "./routes/AuthenticationRouter.js";
 import {mongodbConnection} from "./db.js";
 import dotenv from "dotenv";
 import {passwordRouter} from "./routes/PasswordRouter.js"
-import authVerify from "./middleWare/AuthVerify.js"
+import cors from "cors"
 
 dotenv.config()
 
 const app = express();
+app.use(cors())
 
 mongodbConnection()
 
